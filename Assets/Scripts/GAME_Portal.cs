c550f7768fea7fa328894a64dfc3f1df19149f88
gerritsch <gerritschoene.info@gmail.com>
2016-12-10 14:42:41 +0100

    Added Portal Script + Test Portal Prefab

---
+++ Assets/Scripts/GAME_Portal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GAME_Portal : MonoBehaviour {
+
+    public GameObject portalExit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            float playerYScale = other.gameObject.transform.lossyScale.y;
+            other.gameObject.transform.position = new Vector3(portalExit.transform.position.x, portalExit.transform.position.y + playerYScale, portalExit.transform.position.z);
+        }
+    }
+}
