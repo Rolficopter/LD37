1d660df44e0231ad0aa408bc9dfe898f1e90dfde
Jan-Henrik Bruhn <jh.bruhn@me.com>
2016-12-10 14:04:22 +0100

    Add Test Level, Implement The Dude, Shooting, Bad Guys

---
+++ Assets/Scripts/DudeShootingScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DudeShootingScript : MonoBehaviour {
+
+	public Transform projectile;
+	public float projectileSpeed = 5000;
+	public Transform projectileSpawn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			Transform bullet = Instantiate(projectile, projectileSpawn.position, projectileSpawn.rotation);
+			bullet.GetComponent<Rigidbody>().AddForce (bullet.transform.forward * projectileSpeed);
+			Destroy (bullet.gameObject, 2);
+		}
+	}
+}
