49298fbc56ba2a05e780b93be7a8aa6b2ed116c1
gerritsch <gerritschoene.info@gmail.com>
2016-12-10 12:54:23 +0100

    Added Main Menu

---
+++ Assets/Scripts/UI_MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UI_MainMenu : MonoBehaviour {
+
+	public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
