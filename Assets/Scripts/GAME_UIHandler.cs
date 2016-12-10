898036745b9cfd39849a25e77e77f71a1100fef8
gerritsch <gerritschoene.info@gmail.com>
2016-12-10 13:55:16 +0100

    Added Testing Scene(mine), GameManager
    Game Canvas and GameManager GO in Prefab Folder
    GameManager including KeyHandler,GameHandler, UIHandler

---
+++ Assets/Scripts/GAME_UIHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GAME_UIHandler : MonoBehaviour {
+
+    public Text currentLevelTxt;
+    public Text enemiesKilledTxt;
+	
+    public void updateText()
+    {
+        currentLevelTxt.text = "Current Level: " + GAME_GameManager.getCurrentLevel().ToString();
+        enemiesKilledTxt.text = "Enemies Killed: " + GAME_GameManager.getEnemiesKilled().ToString();
+    }
+
+}
