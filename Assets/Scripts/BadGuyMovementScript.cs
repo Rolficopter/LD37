d4f68ead7f3df9ab6d2e674b4f8d8593fa8bd9fb
Jan-Henrik Bruhn <jh.bruhn@me.com>
2016-12-10 14:29:40 +0100

    Add BadGuyMovement following the dude.

---
+++ Assets/Scripts/BadGuyMovementScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadGuyMovementScript : MonoBehaviour {
+
+	public Transform target;
+	public float speed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Slerp (transform.position, target.position, Time.deltaTime * speed);
+	}
+}
