e33b79e0601d21b29edfbc01f2869c6c03c8fdfd
Jan-Henrik Bruhn <jh.bruhn@me.com>
2016-12-10 23:58:52 +0100

    Limit Bullet Shooting Frequency

--- Assets/Scripts/Player/DudeShootingScript.cs
+++ Assets/Scripts/Player/DudeShootingScript.cs
@@ -8,18 +8,25 @@ public class DudeShootingScript : MonoBehaviour {
 	public float projectileSpeed = 5000;
 	public Transform projectileSpawn;
 
+	private float shootLimit;
+
+	private float lastShoot = 0;
+	private bool shot = false;
+
 	// Use this for initialization
 	void Start () {
-		
+		shootLimit = FindObjectOfType<DudeShootAnimationScript> ().animationDuration;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown ("Fire1")) {
+		if (Input.GetButtonDown ("Fire1") && (Time.time > lastShoot + shootLimit || !shot)) {
 			Transform bullet = Instantiate(projectile, projectileSpawn.position, Camera.main.transform.rotation);
 			//bullet.rotation = Quaternion.Euler(bullet.rotation.eulerAngles.x - 90, bullet.rotation.eulerAngles.y, bullet.rotation.eulerAngles.z);
 			bullet.GetComponent<Rigidbody>().AddForce (bullet.transform.forward * projectileSpeed);
 			Destroy (bullet.gameObject, 2);
+			lastShoot = Time.time;
+			shot = true;
 		}
 	}
 }
