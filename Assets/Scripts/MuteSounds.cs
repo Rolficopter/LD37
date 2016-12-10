361f322b0bb0d47c26bd6678cbc1ae089c810d28
gerritsch <gerritschoene.info@gmail.com>
2016-12-10 22:50:19 +0100

    Added Mute Sounds Script (attached to GameManager)
    - mute/unmute sounds with

---
+++ Assets/Scripts/MuteSounds.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteSounds : MonoBehaviour {
+
+    private bool muted;
+    private float volume;
+
+    // Use this for initialization
+    void Start () {
+        volume = AudioListener.volume;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            muted = !muted;
+            if (muted)
+            {
+                AudioListener.volume = 0f;
+            }else
+            {
+                AudioListener.volume = volume;
+            }
+        }
+	}
+}
