eec5a6c9d746d50ba0424304ddd396c7b66295b1
Jan-Henrik Bruhn <jh.bruhn@me.com>
2016-12-10 21:47:00 +0100

    Add Enemy Tag, DudeHealthScript

---
+++ Assets/DudeHealthScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DudeHealthScript : MonoBehaviour {
+
+	public float maxHealth = 100f;
+	public float health = maxHealth;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
