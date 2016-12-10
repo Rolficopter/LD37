7dbd785b2c6a9e148e4bd28c107821c41b2b56b1
Sören Gade <gade.soeren@gmail.com>
2016-12-10 13:53:40 +0100

    Added level 2 and logic to get to it.
    After 10 seconds the wall disappears to open up for "level 2".

---
+++ Assets/Scripts/TestLevel2Logic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLevel2Logic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (GoToLevel2 ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator GoToLevel2() {
+		yield return new WaitForSeconds (10);
+
+		var objectsToRemove = GameObject.FindGameObjectsWithTag ("RemoveFor_Level2");
+		foreach (var obj in objectsToRemove) {
+			DestroyObject (obj);
+		}
+	}
+}
