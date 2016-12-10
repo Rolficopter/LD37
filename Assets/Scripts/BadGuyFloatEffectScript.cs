ace3baf83704fea6c18bb8d043d1caf0e0cfd494
Jan-Henrik Bruhn <jh.bruhn@me.com>
2016-12-10 14:22:49 +0100

    Add Floating Effect to Bad Guys

---
+++ Assets/Scripts/BadGuyFloatEffectScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadGuyFloatEffectScript : MonoBehaviour {
+
+	public float amplitude = 0.005f;
+	public float frequency = 3;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3(0, amplitude * Mathf.Sin (Time.time * frequency), 0);
+	}
+}
