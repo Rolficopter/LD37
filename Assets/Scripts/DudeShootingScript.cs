f34939e4de8c49f18726ad04830c0bda9310b5d7
Jan-Henrik Bruhn <jh.bruhn@me.com>
2016-12-10 17:26:44 +0100

    Replace Old Bullet with Nais Bullet

--- Assets/Scripts/DudeShootingScript.cs
+++ Assets/Scripts/DudeShootingScript.cs
@@ -17,6 +17,7 @@ public class DudeShootingScript : MonoBehaviour {
 	void Update () {
 		if (Input.GetButtonDown ("Fire1")) {
 			Transform bullet = Instantiate(projectile, projectileSpawn.position, Camera.main.transform.rotation);
+			//bullet.rotation = Quaternion.Euler(bullet.rotation.eulerAngles.x - 90, bullet.rotation.eulerAngles.y, bullet.rotation.eulerAngles.z);
 			bullet.GetComponent<Rigidbody>().AddForce (bullet.transform.forward * projectileSpeed);
 			Destroy (bullet.gameObject, 2);
 		}
