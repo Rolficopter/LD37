de84a986922a7145407ba2750c6e773de096ad89
Jan-Henrik Bruhn <jh.bruhn@me.com>
2016-12-10 22:56:28 +0100

    Implement Player Pushback

--- Assets/Scripts/Enemies/BadGuyMovementScript.cs
+++ Assets/Scripts/Enemies/BadGuyMovementScript.cs
@@ -7,15 +7,32 @@ public class BadGuyMovementScript : MonoBehaviour {
 	public Transform target;
 	public float speed = 0.003f;
 
+	private float lastCollision;
+	private Vector3 backwardsDirection;
+
 	// Use this for initialization
 	void Start () {
-		
+		lastCollision = float.MaxValue;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Vector3 peda = target.position - transform.position;
-		Vector3 newPosition = transform.position + speed * (Vector3.Normalize(peda));
-		transform.position = new Vector3(newPosition.x, transform.position.y, newPosition.z);
+		Vector3 newPosition;
+
+		if (Time.time - lastCollision > 0.5f || Time.time - lastCollision < 0) {
+			newPosition = transform.position + speed * (Vector3.Normalize (peda));
+		} else {
+			newPosition = transform.position - speed * 3 * (Vector3.Normalize (peda));
+		}
+	
+		Rigidbody rigidbody = GetComponent<Rigidbody> ();
+		rigidbody.MovePosition (newPosition);
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.CompareTag ("Player")) {
+			lastCollision = Time.time;
+		}
 	}
 }
--- Assets/Scripts/Player/DudeHealthScript.cs
+++ Assets/Scripts/Player/DudeHealthScript.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class DudeHealthScript : MonoBehaviour {
 
 	public float maxHealth = 100f;
-	public float health = maxHealth;
+	public float health = 0f;
 
 	// Use this for initialization
 	void Start () {
-		
+		health = maxHealth;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.CompareTag ("Enemy")) {
+			health -= 2;
+			// Play Player Hurt Audio.
+		}
 	}
 }
