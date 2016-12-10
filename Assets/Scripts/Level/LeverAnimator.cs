94ea0ed02dab4dfdc9b44082eb0ec2583d4e4491
Sören Gade <gade.soeren@gmail.com>
2016-12-10 22:54:13 +0100

    Added lever with animation.

---
+++ Assets/Scripts/Level/LeverAnimator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverAnimator : MonoBehaviour {
+
+	public string pullLeverAnimationName = "Lever_pull";
+
+	private bool wasAnimated;
+
+	// Use this for initialization
+	void Start () {
+		wasAnimated = false;
+	}
+
+	/// <summary>
+	/// Runs the pull animation.
+	/// </summary>
+	public void RunPullAnimation() {
+		if (this.wasAnimated) {
+			return;
+		}
+		this.wasAnimated = true;
+
+		this.gameObject.GetComponent<Animation>().Play (this.pullLeverAnimationName);
+	}
+}
---
+++ Assets/Scripts/Player/DudeInteractionManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DudeInteractionManager : MonoBehaviour {
+
+	[Range(0, 10)]
+	public float interactionDistance = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		if (Input.GetKeyUp (KeyCode.E)) {
+			RaycastHit hit;
+			Vector3 forward = this.transform.TransformDirection (Vector3.forward);
+			if (Physics.Raycast (transform.position, forward, out hit, this.interactionDistance)) {
+				//Debug.LogFormat ("Hit {0} at distance {1}", hit.collider.gameObject, hit.distance);
+
+				GameObject lever = hit.collider.gameObject;
+				if (lever.tag == "Lever") {
+					var animator = lever.GetComponent<LeverAnimator> ();
+					if (animator != null) {
+						animator.RunPullAnimation ();
+					}
+				}
+			}
+		}
+	}
+}
