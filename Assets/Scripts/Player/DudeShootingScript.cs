a6667814f7a0ffd23aed0d9aab690841bac9bdc0
cjmoeller <the.porenta@web.de>
2016-12-11 14:17:36 +0100

    added flash when shooting

--- Assets/Scripts/Player/DudeShootingScript.cs
+++ Assets/Scripts/Player/DudeShootingScript.cs
@@ -9,18 +9,33 @@ public class DudeShootingScript : MonoBehaviour {
 	public Transform projectileSpawn;
 
 	private float shootLimit;
+    private float shootingLightDuration = 0.05f;
 
 	private float lastShoot = 0;
 	private bool shot = false;
+    private bool lightOn = false;
+
+
 
 	// Use this for initialization
 	void Start () {
 		shootLimit = FindObjectOfType<DudeShootAnimationScript> ().animationDuration;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    private float deltaTimeSum = 0f;
+    void Update () {
+        deltaTimeSum += Time.deltaTime;
 		if (Input.GetButtonDown ("Fire1") && (Time.time > lastShoot + shootLimit || !shot)) {
+            var lights = this.GetComponentsInChildren<Light>();
+            foreach (Light light in lights) {
+                Debug.Log(light.name);
+                if (light.name.Equals("Point light")) {
+                    light.intensity = 3f;
+                    deltaTimeSum = 0f;
+                    lightOn = true;
+                }
+            }
 			Transform bullet = Instantiate(projectile, projectileSpawn.position, Camera.main.transform.rotation);
 			//bullet.rotation = Quaternion.Euler(bullet.rotation.eulerAngles.x - 90, bullet.rotation.eulerAngles.y, bullet.rotation.eulerAngles.z);
 			bullet.GetComponent<Rigidbody>().AddForce (bullet.transform.forward * projectileSpeed);
@@ -28,5 +43,17 @@ public class DudeShootingScript : MonoBehaviour {
 			lastShoot = Time.time;
 			shot = true;
 		}
+        if (lightOn && deltaTimeSum >= this.shootingLightDuration) {
+            var lights = this.GetComponentsInChildren<Light>();
+            foreach (Light light in lights)
+            {
+                if (light.name.Equals("Point light"))
+                {
+                    light.intensity = 0;                  
+                }
+            }
+
+            lightOn = false;
+        }
 	}
 }
